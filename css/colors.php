98c109fc37e32d6863d24bff272f50964d4b7b99
Stephen Shutters <stephencshutters@gmail.com>
2020-01-25 16:13:10 -0500

    Added more CSS files.

--- css/colors.php
+++ css/colors.php
@@ -20,7 +20,7 @@ $color = "#1ABC9C"; // Change your Color Here
 
 function checkhexcolor($color) {
 
-    return preg_match('/^#[a-f0-9]{6}$/i', $color);
+	return preg_match('/^#[a-f0-9]{6}$/i', $color);
 
 }
 
@@ -31,20 +31,20 @@ function checkhexcolor($color) {
  ================================================================== */
 
 if( isset( $_GET[ 'color' ] ) AND $_GET[ 'color' ] != '' ) {
-    $color = "#" . $_GET[ 'color' ];
+	$color = "#" . $_GET[ 'color' ];
 }
 
 if( !$color OR !checkhexcolor( $color ) ) {
-    $color = "#1ABC9C";
+	$color = "#1ABC9C";
 }
 
 ?>
 
 
 /* ----------------------------------------------------------------
-    Colors
+	Colors
 
-    Replace the HEX Code with your Desired Color HEX
+	Replace the HEX Code with your Desired Color HEX
 -----------------------------------------------------------------*/
 
 
@@ -63,14 +63,16 @@ h4 > span:not(.nocolor):not(.badge),
 h5 > span:not(.nocolor):not(.badge),
 h6 > span:not(.nocolor):not(.badge),
 .header-extras li .he-text span,
-#primary-menu > ul > li:hover a,
-#primary-menu > ul > li.current a,
-#primary-menu > div > ul > li:hover a,
-#primary-menu > div > ul > li.current a,
+#primary-menu ul li:hover > a,
+#primary-menu ul li.current > a,
+#primary-menu div ul li:hover > a,
+#primary-menu div ul li.current > a,
+#primary-menu ul ul li:hover > a,
+#primary-menu ul li .mega-menu-content.style-2 ul.mega-menu-column > li.mega-menu-title > a:hover,
 #top-cart > a:hover,
 .top-cart-action span.top-checkout-price,
 .breadcrumb a:hover,
-#portfolio-filter li a:hover,
+.portfolio-filter li a:hover,
 .portfolio-desc h3 a:hover,
 .portfolio-overlay a:hover,
 #portfolio-navigation a:hover,
@@ -97,12 +99,12 @@ h6 > span:not(.nocolor):not(.badge),
 .pricing-box.best-price .pricing-price,
 .btn-link,
 .dark .post-timeline .entry:hover .entry-timeline,
-.dark .post-timeline .entry:hover .timeline-divider { color: <?php echo $color; ?>; }
+.dark .post-timeline .entry:hover .timeline-divider,
+.clear-rating-active:hover { color: <?php echo $color; ?>; }
 
 .color,
-#primary-menu ul ul li:hover > a,
-#primary-menu ul li .mega-menu-content.style-2 > ul > li.mega-menu-title > a:hover,
 .top-cart-item-desc a:hover,
+.portfolio-filter.style-3 li.activeFilter a,
 .faqlist li a:hover,
 .tagcloud a:hover,
 .dark .top-cart-item-desc a:hover,
@@ -110,7 +112,10 @@ h6 > span:not(.nocolor):not(.badge),
 .dark.overlay-menu #header-wrap:not(.not-dark) #primary-menu > ul > li:hover > a,
 .dark.overlay-menu #header-wrap:not(.not-dark) #primary-menu > ul > li.current > a,
 .overlay-menu #primary-menu.dark > ul > li:hover > a,
-.overlay-menu #primary-menu.dark > ul > li.current > a { color: <?php echo $color; ?> !important; }
+.overlay-menu #primary-menu.dark > ul > li.current > a,
+.nav-tree li:hover > a,
+.nav-tree li.current > a,
+.nav-tree li.active > a { color: <?php echo $color; ?> !important; }
 
 #primary-menu.style-3 > ul > li.current > a,
 #primary-menu.sub-title > ul > li:hover > a,
@@ -119,13 +124,15 @@ h6 > span:not(.nocolor):not(.badge),
 #primary-menu.sub-title > div > ul > li.current > a,
 #top-cart > a > span,
 #page-menu-wrap,
+#page-menu ul ul,
 #page-menu.dots-menu nav li.current a,
-#page-menu.dots-menu nav li .dots-menu-caption,
-#portfolio-filter li.activeFilter a,
-#portfolio-shuffle:hover,
+#page-menu.dots-menu nav li div,
+.portfolio-filter li.activeFilter a,
+.portfolio-filter.style-4 li.activeFilter a:after,
+.portfolio-shuffle:hover,
 .entry-link:hover,
 .sale-flash,
-.button:not(.button-white):not(.button-dark):not(.button-border):not(.button-black):not(.button-red):not(.button-teal):not(.button-yellow):not(.button-green):not(.button-brown):not(.button-aqua):not(.button-purple):not(.button-leaf):not(.button-pink):not(.button-blue):not(.button-dirtygreen):not(.button-amber):not(.button-lime),
+.button:not(.button-white):not(.button-dark):not(.button-border):not(.button-black):not(.button-red):not(.button-teal):not(.button-yellow):not(.button-green):not(.button-brown):not(.button-aqua):not(.button-purple):not(.button-leaf):not(.button-pink):not(.button-blue):not(.button-dirtygreen):not(.button-amber):not(.button-lime):not(:hover),
 .button.button-dark:hover,
 .promo.promo-flat,
 .feature-box .fbox-icon i,
@@ -138,7 +145,7 @@ h6 > span:not(.nocolor):not(.badge),
 ul.tab-nav.tab-nav2 li.ui-state-active a,
 .testimonial .flex-control-nav li a,
 .skills li .progress,
-.owl-theme .owl-dots .owl-dot,
+.owl-carousel .owl-dots .owl-dot,
 #gotoTop:hover,
 .dark .button-dark:hover,
 .dark .fbox-effect.fbox-dark .fbox-icon i:hover,
@@ -148,7 +155,21 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 .dark .i-circled:hover,
 .dark ul.tab-nav.tab-nav2 li.ui-state-active a,
 .dark .tagcloud a:hover,
-.ei-slider-thumbs li.ei-slider-element { background-color: <?php echo $color; ?>; }
+.ei-slider-thumbs li.ei-slider-element,
+.nav-pills .nav-link.active,
+.nav-pills .nav-link.active:hover,
+.nav-pills .nav-link.active:focus,
+.nav-pills .show > .nav-link,
+.checkbox-style:checked + .checkbox-style-1-label:before,
+.checkbox-style:checked + .checkbox-style-2-label:before,
+.checkbox-style:checked + .checkbox-style-3-label:before,
+.radio-style:checked + .radio-style-3-label:before,
+input.switch-toggle-flat:checked + label,
+input.switch-toggle-flat:checked + label:after,
+input.switch-toggle-round:checked + label:before,
+.bootstrap-switch .bootstrap-switch-handle-on.bootstrap-switch-themecolor,
+.bootstrap-switch .bootstrap-switch-handle-off.bootstrap-switch-themecolor,
+.entry:after { background-color: <?php echo $color; ?>; }
 
 .bgcolor,
 .button.button-3d:not(.button-white):not(.button-dark):not(.button-border):not(.button-black):not(.button-red):not(.button-teal):not(.button-yellow):not(.button-green):not(.button-brown):not(.button-aqua):not(.button-purple):not(.button-leaf):not(.button-pink):not(.button-blue):not(.button-dirtygreen):not(.button-amber):not(.button-lime):hover,
@@ -156,14 +177,22 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 .process-steps li.ui-tabs-active a,
 .sidenav > .ui-tabs-active > a,
 .sidenav > .ui-tabs-active > a:hover,
-.owl-theme .owl-controls .owl-nav [class*=owl-]:hover,
+.owl-carousel .owl-nav [class*=owl-]:hover,
 .page-item.active .page-link,
 .page-link:hover,
-.page-link:focus { background-color: <?php echo $color; ?> !important; }
+.page-link:focus,
+.irs-bar,
+.irs-from,
+.irs-to,
+.irs-single,
+.irs-handle > i:first-child,
+.irs-handle.state_hover > i:first-child,
+.irs-handle:hover > i:first-child { background-color: <?php echo $color; ?> !important; }
 
 #primary-menu.style-4 > ul > li:hover > a,
 #primary-menu.style-4 > ul > li.current > a,
 .top-cart-item-image:hover,
+.portfolio-filter.style-3 li.activeFilter a,
 .post-timeline .entry:hover .entry-timeline,
 .post-timeline .entry:hover .timeline-divider,
 .cart-product-thumbnail img:hover,
@@ -177,7 +206,7 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 
 .top-links ul ul,
 .top-links ul div.top-link-section,
-#primary-menu ul ul,
+#primary-menu ul ul:not(.mega-menu-column),
 #primary-menu ul li .mega-menu-content,
 #primary-menu.style-6 > ul > li > a:after,
 #primary-menu.style-6 > ul > li.current > a:after,
@@ -189,9 +218,14 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 #primary-menu.dark ul li .mega-menu-content,
 .dark #primary-menu:not(.not-dark) ul li .mega-menu-content.style-2,
 #primary-menu.dark ul li .mega-menu-content.style-2,
-.dark #top-cart .top-cart-content { border-top-color: <?php echo $color; ?>; }
+.dark #top-cart .top-cart-content,
+.tabs.tabs-tb ul.tab-nav li.ui-tabs-active a { border-top-color: <?php echo $color; ?>; }
 
-#page-menu.dots-menu nav li .dots-menu-caption:after,
+.irs-from:before,
+.irs-to:before,
+.irs-single:before { border-top-color: <?php echo $color; ?> !important; }
+
+#page-menu.dots-menu nav li div:after,
 .title-block { border-left-color: <?php echo $color; ?>; }
 
 .title-block-right { border-right-color: <?php echo $color; ?>; }
@@ -202,7 +236,8 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 .fancy-title.title-bottom-border h4,
 .fancy-title.title-bottom-border h5,
 .fancy-title.title-bottom-border h6,
-.more-link { border-bottom-color: <?php echo $color; ?>; }
+.more-link,
+.tabs.tabs-bb ul.tab-nav li.ui-tabs-active a { border-bottom-color: <?php echo $color; ?>; }
 
 .border-color,
 .process-steps li.active a,
@@ -223,21 +258,20 @@ ul.tab-nav.tab-nav2 li.ui-state-active a,
 
 @media only screen and (max-width: 991px) {
 
-    body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > ul > li:hover a,
-    body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > ul > li.current a,
-    body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > div > ul > li:hover a,
-    body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > div > ul > li.current a,
-    #primary-menu ul ul li:hover > a,
-    #primary-menu ul li .mega-menu-content.style-2 > ul > li.mega-menu-title:hover > a,
-    #primary-menu ul li .mega-menu-content.style-2 > ul > li.mega-menu-title > a:hover { color: <?php echo $color; ?> !important; }
+	body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > ul > li:hover a,
+	body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > ul > li.current a,
+	body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > div > ul > li:hover a,
+	body:not(.dark) #header:not(.dark) #header-wrap:not(.dark) #primary-menu > div > ul > li.current a,
+	#primary-menu ul ul li:hover > a,
+	#primary-menu ul li .mega-menu-content.style-2 > ul > li.mega-menu-title:hover > a,
+	#primary-menu ul li .mega-menu-content.style-2 > ul > li.mega-menu-title > a:hover { color: <?php echo $color; ?> !important; }
 
-    #page-menu nav { background-color: <?php echo $color; ?>; }
+	#page-menu nav { background-color: <?php echo $color; ?>; }
 
 }
 
 
 @media only screen and (max-width: 767px) {
 
-    #portfolio-filter li a:hover { color: <?php echo $color; ?>; }
-
-}
+	.portfolio-filter li a:hover { color: <?php echo $color; ?>; }
+.}
